8eb215aea57e271a689934b712698c7abf0e7f8e
HyagoColaresMM <hyago.colares.mminformatica@google.com>
2025-02-26 11:48:09 -0300

    Corrigindo a paginação

--- src/services/fabcomptipomaterial.service.js
+++ src/services/fabcomptipomaterial.service.js
@@ -7,7 +7,9 @@ import {
 
 const Listar = async (descricao, limit, skip) => {
     return new Promise((resolve, reject) => {
-        let ssql = 'SELECT ID, DESCRICAO, CREATED_AT, UPDATED_AT FROM FABCOMP_TIPOMATERIAL WHERE DELETED_AT IS NULL ';
+        let ssql = 'SELECT';
+        ssql += ` FIRST ${limit} SKIP ${skip}`
+        ssql += ' ID, DESCRICAO, CREATED_AT, UPDATED_AT FROM FABCOMP_TIPOMATERIAL WHERE DELETED_AT IS NULL ';
         const params = [];
 
         if (descricao) {
